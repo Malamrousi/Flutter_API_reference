45dfb7ef0bba8ba06d57bc07ab7c5a81a37a0d16
Mohammed AL-amrousi <m.alamrousi.work2@gmail.com>
2024-08-24 18:17:49 +0300

    create cubit and state function

---
+++ lib/cubit/my_cubit.dart
@@ -0,0 +1,17 @@
+import 'package:api_learn/my_repo.dart';
+import 'package:api_learn/users_models.dart';
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'my_state.dart';
+
+class MyCubit extends Cubit<MyState> {
+  final MyRepo myRepo;
+  MyCubit({required this.myRepo}) : super(MyInitial());
+
+  emitGetAllUser() {
+    myRepo
+        .getAllUsersModelsRepo()
+        .then((getUserList) => emit(GetAllUsers(allUserList: getUserList)));
+  }
+}
---
+++ lib/cubit/my_state.dart
@@ -0,0 +1,14 @@
+part of 'my_cubit.dart';
+
+@immutable
+sealed class MyState {}
+
+final class MyInitial extends MyState {}
+
+final class GetAllUsers extends MyState {
+  final List<UsersModels> allUserList;
+
+  GetAllUsers({
+    required this.allUserList,
+  });
+}
